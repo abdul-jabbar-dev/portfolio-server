222b343e39b8da7c2e875ee66c258b27fa0605b7
abdul-jabbar-dev <abduljabbardev@gmail.com>
2025-10-06 21:01:20 +0600

    deno project init

--- config/path.ts
+++ config/path.ts
@@ -1,3 +1,4 @@
+export const DATABASE_URL="postgresql://postgres.pyoaowwarxwvraghsvpz:Devabdul39@aws-1-ap-southeast-1.pooler.supabase.com:6543/postgres"
 const ROOT = "/";
 const GQL = "/gql";
 const path = {
